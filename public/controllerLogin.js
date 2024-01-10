86cb9a51cda09bc52b1ab2d9cee83d573c73095e
baoco1205 <baoco1205@gmail.com>
2024-01-10 09:27:17 +0700

    10/1/2024

--- public/controllerLogin.js
+++ public/controllerLogin.js
@@ -29,10 +29,41 @@ $(document).ready(() => {
         let token = data.token;
         localStorage.setItem("token", token);
         // axios.defaults.headers.common["Bearer Token"] = `Bearer ${token}`;
+        ///////
+        let getToken = localStorage.getItem(token);
+        console.log(getToken);
+
+        fetch("http://localhost:3000/homepage", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer + ${getToken}`,
+          },
+          body: JSON.stringify({}),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error(
+              "There was a problem with the fetch operation:",
+              error
+            );
+          });
+        ///////
         location.href = "http://localhost:3000/homepage";
       })
       .catch((error) => {
         console.error("There was a problem with the fetch operation:", error);
       });
   });
+
+  /////
+  /////
 });
